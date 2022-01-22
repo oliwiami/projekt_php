74e08fa258acdbdfd39b4b5aadf54fd4a1ed83f5
Oliwia Mi <o.mierkulow@gmail.com>
2022-01-22 22:21:19 +0100

    commit

--- src/profile.php
+++ src/profile.php
@@ -7,7 +7,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
-
+<body>
+<div class="profile">
+<h3>
 <?php
 session_start();
 
@@ -21,27 +23,24 @@ $pesel = $_SESSION['pesel'];
     $result = $connect -> query($dataRead);
     if($result){
         while ($row = $result->fetch_assoc()){
-            echo "Imie i nazwisko: </br>";
+            echo "Imie i nazwisko: ";
             echo $row["imie"] ." ". $row["nazwisko"]."</br>";
-            echo "Numer indeksu: </br>";
+            echo "Numer indeksu: ";
             echo $row["numer_indeksu"] . "</br>";
         }
         $result->free();
     }
-   
-
-    echo "--------------------------------------------------------------"."</br>";
-
     
 ?>
+</h3>
 
-<body>
 </br>
-     <a href="lectures.php"> Moje wykłady </a></br>
+     <a href="lectures.php" id="lecBtn"> Moje wykłady </a></br>
 </br>
-     <a href="enroll.php">Zapisz się na wykład</a></br>
+     <a href="enroll.php" id="enrollBtn">Zapisz się na wykład</a></br>
 </br>
-     <a href="logout.php">Wyloguj </a></br>
+     <a href="logout.php" id="logout">Wyloguj </a></br>
+</div>
 </body>
 
 </html>
